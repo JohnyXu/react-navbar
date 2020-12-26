5ce76f03084977d279caec151f931dbc61ecccb2
Johny Xu <ahstuxq@163.com>
2020-12-27 09:11:25 +1100

    add init navbar components and switch day & night

---
+++ README.md
@@ -0,0 +1,18 @@
+## React Responsive Navbar
+
+This project uses the following technologies:
+
+- [React](https://reactjs.org) for frontend
+- [Styled Components](https://styled-components.com/) for styling the app
+
+## Quick Start
+
+```javascript
+// install dependencies
+yarn
+
+// start
+yarn start
+
+// Server runs on http://localhost:3000
+```
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import styled, { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import { MenuProvider } from './state';
+import Navbar from './components/nav';
+
+// routes
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+
+  html {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Montserrat", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: var(--bg);
+    color: var(--text);
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+const ContentWrapper = styled.div`
+  margin-top: 64px;
+  height: 100vh;
+`;
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <GlobalStyle />
+        <MenuProvider>
+          <Navbar />
+        </MenuProvider>
+        <ContentWrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </ContentWrapper>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
